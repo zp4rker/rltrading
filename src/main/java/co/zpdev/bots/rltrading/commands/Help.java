3de1cff83f32a153d00829a7832f81fea48f139d
ZP4RKER <zp4rker@programmer.net>
2018-03-03 23:28:00 +1100

    added help command

---
+++ src/main/java/co/zpdev/bots/rltrading/commands/Help.java
@@ -0,0 +1,20 @@
+package co.zpdev.bots.rltrading.commands;
+
+import co.zpdev.bots.core.command.Command;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageEmbed;
+
+import java.awt.*;
+
+public class Help {
+
+    @Command(aliases = "help")
+    public void onCommand(Message message) {
+        MessageEmbed embed = new EmbedBuilder().setColor(Color.RED).setAuthor("Command List")
+                .setFooter("Developed by ZP4RKER#3333", null)
+                .setDescription("**Main Commands**\n!topinvites\n!8ball\n!help\n!credits\n!invite\n" +
+                                            "**Staff Commands**\n!ban\n!kick\n!resetinvites").build();
+    }
+
+}
